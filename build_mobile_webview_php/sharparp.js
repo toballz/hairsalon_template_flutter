531f13a6dc6a60b6275930965206c3e7adbf52e9
toballz <42551340+toballz@users.noreply.github.com>
2024-07-17 20:45:38 -0700

    Delete build_mobile_webview_php directory

--- build_mobile_webview_php/sharparp.js
+++
@@ -1,9 +0,0 @@
-var schema="jk",dhost="159742f243a05f0733d5d6497fd3f947",d="/",datt=[], sharparp={
-    push: function(datt){
-        /*{
-            title: til,
-            value: val
-        } */
-        window.location.href= schema+":"+d+d+dhost+d+btoa(JSON.stringify(datt));
-    }, 
-}
